fe0b1273db9ef0556010157ddb8457485b1cb2b2
duckie-boy <john.kampman2@gmail.com>
2024-03-24 01:49:59 -0400

    CameraWorking

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,7 +15,7 @@ public class CameraFollow : MonoBehaviour
     }
 
     public void follow() {
-        Vector3 desiredPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+        Vector3 desiredPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
         if(target.transform.localScale.x > 0.0f) {
             desiredPosition = new Vector3(desiredPosition.x + offset, desiredPosition.y, desiredPosition.z);
         } else if(target.transform.localScale.x < 0.0f) {
