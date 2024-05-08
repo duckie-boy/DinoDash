334097fd4bbfbbd25d0c401fb196e4c9accad4dc
duckie-boy <john.kampman2@gmail.com>
2024-05-07 22:13:35 -0500

    Bug Fixes

--- Assets/Scripts/levelLocker.cs
+++ Assets/Scripts/levelLocker.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class levelLocker : MonoBehaviour
 {
     [SerializeField] Button[] levelButtons;
+    public int levelAt;
     void Start()
     {
-        int levelAt = PlayerPrefs.GetInt("levelAt", 1);
+        PlayerPrefs.DeleteAll();
+        levelAt = PlayerPrefs.GetInt("levelAt", 1);
 
-        for(int i = 0; i < levelButtons.Length; i++) {
+        for(int i = 1; i < levelButtons.Length; i++) {
             if(i + 1 > levelAt) {
                 levelButtons[i].interactable = false;
             }
